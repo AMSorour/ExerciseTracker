8e77b37ad741f6dac24cee4468e56984305d215d
AMSorour <54924721+AMSorour@users.noreply.github.com>
2021-04-25 12:28:19 -0400

    Pushing Mern Stack Code

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {BrowserRouter as Router, Route} from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css"
+
+import Navbar from "./components/navbar"
+import ExercisesList from "./components/exerciseList";
+import EditExercise from "./components/editExercise";
+import CreateExercise from "./components/createExercise";
+import CreateUser from "./components/createUser";
+
+function App() {
+  return (
+    <Router>
+      <div className="container">
+      <Navbar/>
+      <br/>
+      <Route path="/" exact component={ExercisesList}/>
+      <Route path="/edit/:id" exact component={EditExercise}/>
+      <Route path="/create" exact component={CreateExercise}/>
+      <Route path="/user" exact component={CreateUser}/>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
